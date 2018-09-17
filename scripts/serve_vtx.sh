a7464c1ff9d596ebbea08c7798d16d43ad23ce70
Jason Thomas <mail@jasonthom.as>
2018-09-16 20:27:54 -0600

    Fix support for non-SSL, specifically so we can use an external SSL termination strategy.

---
+++ scripts/serve_vtx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WS_PROTO="wss"
+if [ "$SUPPORT_SSL" = "false" ]; then
+  WS_PROTO="ws"
+fi
+
+WS_DOMAIN="bbs.localtest.com"
+if ! [ -z "$OVERRIDE_CERT_DOMAIN" ]; then
+  WS_DOMAIN="$OVERRIDE_CERT_DOMAIN"
+fi
+
+WS_PORT=7003
+if ! [ -z "WS_PORT" ]; then
+  WS_PORT=$WS_PORT
+fi
+
+sed -i "s/WEBSOCKET_ADDRESS/${WS_PROTO}:\/\/${WS_DOMAIN}:${WS_PORT}/g" ./vtxclient/vtxdata.js
+
+if [ "$SUPPORT_SSL" = "false" ]; then
+  ./node_modules/http-server/bin/http-server ./vtxclient/
+else
+  ./node_modules/http-server/bin/http-server -S -C ./cert/server.cert -K ./cert/server.key ./vtxclient/
+fi
---
+++ vtxclient/vtxdata.js
@@ -0,0 +1,18 @@
+var vtxdata = {
+  sysName: "Xibalba",
+  wsConnect: "WEBSOCKET_ADDRESS",
+  term: "ansi-bbs",
+  codePage: "CP437",
+  fontName: "UVGA16",
+  fontSize: "24px",
+  crtCols: 80,
+  crtRows: 25,
+  crtHistory: 500,
+  xScale: 1,
+  initStr: "",
+  defPageAttr: 0x1010,
+  defCrsrAttr: 0x0207,
+  defCellAttr: 0x0007,
+  telnet: 1,
+  autoConnect: 0
+};
