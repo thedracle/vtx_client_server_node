040851da35c5036a9cae84aa3588e1e7f43f3f5f
Jason Thomas <mail@jasonthom.as>
2018-09-18 22:53:56 -0600

    Add mechanism to override websocket address completely.

--- scripts/serve_vtx.sh
+++ scripts/serve_vtx.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-WS_PROTO="wss"
-if [ "$SUPPORT_SSL" = "false" ]; then
-  WS_PROTO="ws"
-fi
+if ! [ -z "$WEBSOCKET_OVERRIDE_ADDRESS" ]; then
+  sed -i "s/WEBSOCKET_ADDRESS/${WEBSOCKET_OVERRIDE_ADDRESS}/g" ./vtxclient/vtxdata.js
+else
+  WS_PROTO="wss"
+  if [ "$SUPPORT_SSL" = "false" ]; then
+    WS_PROTO="ws"
+  fi
 
-WS_DOMAIN="bbs.localtest.com"
-if ! [ -z "$OVERRIDE_CERT_DOMAIN" ]; then
-  WS_DOMAIN="$OVERRIDE_CERT_DOMAIN"
-fi
+  WS_DOMAIN="bbs.localtest.com"
+  if ! [ -z "$OVERRIDE_CERT_DOMAIN" ]; then
+    WS_DOMAIN="$OVERRIDE_CERT_DOMAIN"
+  fi
 
-WS_PORT=7003
-if ! [ -z "WS_PORT" ]; then
-  WS_PORT=$WS_PORT
-fi
+  WS_PORT=7003
+  if ! [ -z "WS_PORT" ]; then
+    WS_PORT=$WS_PORT
+  fi
 
-sed -i "s/WEBSOCKET_ADDRESS/${WS_PROTO}:\/\/${WS_DOMAIN}:${WS_PORT}/g" ./vtxclient/vtxdata.js
+  sed -i "s/WEBSOCKET_ADDRESS/${WS_PROTO}:\/\/${WS_DOMAIN}:${WS_PORT}/g" ./vtxclient/vtxdata.js
+fi
 
 if [ "$SUPPORT_SSL" = "false" ]; then
   ./node_modules/http-server/bin/http-server ./vtxclient/
